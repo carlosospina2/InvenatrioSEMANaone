ca4af8c62031210c763f2a865a37f06230c10f50
kevin <kevin@gmail.com>
2026-03-05 13:09:27 -0500

    se agrego unos print para el menu

--- menu-interactivo.py
+++ menu-interactivo.py
@@ -2,3 +2,11 @@ from validacion_entrada import validar_monto
 from historial import historial_operaciones
 
 
+def menu(cuenta):
+    print("=" * 40)
+    print("                 Menu")
+    print("=" * 40)
+    print("1- Consultar Saldo")
+    print("2- Retiro de saldo")
+    print("3- Depositar Dinero")
+    Ejecucion = int(input("Elija una opcion"))
